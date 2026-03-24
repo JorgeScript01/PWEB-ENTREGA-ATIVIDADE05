85909422a2a4edc24643af27d019eb9ada98e086
Jorge Matheus Bomfim <matheusblopes04@gmail.com>
2026-03-23 22:34:20 -0300

    Create database.js

---
+++ src/database/database.js
@@ -0,0 +1,15 @@
+// src/database/database.js
+export class Database {
+  constructor() {
+    this.entregas = [];
+    this.nextId = 1;
+  }
+
+  getEntregas() {
+    return this.entregas;
+  }
+
+  generateId() {
+    return this.nextId++;
+  }
+}
